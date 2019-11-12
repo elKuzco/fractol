bc6312111e94ebc15e21b43e5aa0d27a4d0969eb
Quentin LOUISIADE <qlouisia@e3r9p20.42.fr>
2019-11-12 17:14:45 +0100

    working on center camera

--- src/inputs.c
+++ src/inputs.c
@@ -6,7 +6,7 @@
 /*   By: qlouisia <qlouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 14:20:19 by qlouisia          #+#    #+#             */
-/*   Updated: 2019/11/04 18:54:12 by qlouisia         ###   ########.fr       */
+/*   Updated: 2019/11/12 17:13:29 by qlouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,48 @@ int mouse_control(int m_code, int x, int y, t_lst_display **win)
 {
 	printf("Key :%d\n",m_code);
 	//printf("x :%d | y :%d\n",x,y);
-
+	double tmp;
 	
 	//(*win)->x = 0.3;
+	if (m_code == 1)
+	{
+	tmp = ((*win)->Maxreal  - x) * (*win)->Real_scale;
+	//tmp = ( x - (*win)->w_width /(*win)->Maxreal - (*win)->Minreal) * (*win)->Real_scale;
+	//tmp = x * (*win)->Real_scale;
+	printf("tmp :%f \n",tmp);
+	(*win)->Minreal += tmp; 
+	(*win)->Maxreal += tmp; 
+	tmp = ((*win)->Maxima  - y) * (*win)->Ima_scale;
+	//tmp = ( y - (*win)->w_height /(*win)->Maxima - (*win)->Minima) * (*win)->Ima_scale;
+	//tmp = y * (*win)->Ima_scale;
+	(*win)->Minima+= tmp;
+	(*win)->Maxima = (*win)->Minima +((*win)->Maxreal - (*win)->Minreal) * ((*win)->w_height / (*win)->w_width); 
+	(*win)->Real_scale = ((*win)->Maxreal - (*win)->Minreal) / (*win)->w_width; // (*win)->w_width - 1
+	(*win)->Ima_scale = ((*win)->Maxima - (*win)->Minima) / (*win)->w_height; // (*win)->w_height - 1
+	
+	printf("minreal :%f | Max_real :%f\n",(*win)->Minreal,(*win)->Maxreal );
+	printf("minima :%f | Max_ima :%f\n",(*win)->Minima,(*win)->Maxima);
+	printf("Real_scale :%f | Ima_scale :%f\n",(*win)->Real_scale,(*win)->Ima_scale);
+	}
 	if (m_code == 5)
 	{
+		/*
 		(*win)->x = x - ( x - (*win)->w_width /2) * 1.1;
 		(*win)->y = y - ( y - (*win)->w_height /2) * 1.1;
 		printf("x :%f | y :%f\n",(*win)->x,(*win)->y );
 		(*win)->zoom_scale *= 1.1;
 		(*win)->Max_it += 3;
-		printf("Zoom :%f\n",(*win)->zoom_scale);
+		printf("Zoom :%f\n",(*win)->zoom_scale);*/
 	}
 	if (m_code == 4 && ((*win)->zoom_scale > 1))
 	{
+		/*
 		(*win)->x = x - ( x - (*win)->w_width/2) / 1.1;
 		(*win)->y = y - ( y - (*win)->w_height/2) / 1.1;
 		printf("x :%f | y :%f\n",(*win)->x,(*win)->y );
 		(*win)->zoom_scale /= 1.1;
 		(*win)->Max_it -= 3;
-		printf("Zoom :%f\n",(*win)->zoom_scale);
+		printf("Zoom :%f\n",(*win)->zoom_scale);*/
 	}
 	
 	refresh_image(win);
--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: qlouisia <qlouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/10 15:52:03 by qlouisia          #+#    #+#             */
-/*   Updated: 2019/11/04 18:40:50 by qlouisia         ###   ########.fr       */
+/*   Updated: 2019/11/12 15:12:33 by qlouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,129 @@ void initialise_fractal_mandel(t_lst_display **win)
 	(*win)->Maxima = (*win)->Minima +((*win)->Maxreal - (*win)->Minreal) * ((*win)->w_height / (*win)->w_width); 
 	(*win)->Real_scale = ((*win)->Maxreal - (*win)->Minreal) / (*win)->w_width; // (*win)->w_width - 1
 	(*win)->Ima_scale = ((*win)->Maxima - (*win)->Minima) / (*win)->w_height; // (*win)->w_height - 1
-	(*win)->Max_it = 300;
+	(*win)->Max_it = 30;
 	(*win)->pt_function = &mandelbrot;
 	(*win)->color = convert_rgb(0x0,255,0,0);
+	(*win)->x = (*win)->w_width / 2;
+	(*win)->y = (*win)->w_height / 2;
+}
+ 
+
+void mandelbrot(t_lst_display **win)
+{
+	int x;
+	int y;
+	int i;
+	double c_im;
+	double c_re;
+	t_bool isin;
+	double Z_re;
+	double Z_im;
+	double Z_re2;
+	double Z_im2;
+	unsigned int color;
+
+	y = 0;
+	
+	while (y < (*win)->w_height)
+	{
+		c_im = (*win)->Maxima - y * (*win)->Ima_scale;
+		x = 0;
+		while (x < (*win)->w_width)
+		{
+				c_re = (*win)->Minreal + x * (*win)->Real_scale;
+				isin = true;
+				i = 0;
+				Z_re = c_re;
+				Z_im = c_im;
+				while (i < (*win)->Max_it)
+				{
+					Z_re2 = pow(Z_re,2);
+					Z_im2 = pow(Z_im,2);
+					if (Z_re2 + Z_im2  > 4)
+					{
+						break;
+					}
+					Z_im = 2 * Z_re * Z_im + c_im;
+					Z_re = Z_re2 - Z_im2 + c_re;
+					i++;
+				}
+				color = convert_rgb(0x0, 255, 255,255 * (i < (*win)->Max_it));
+				fill_pix(win,x,y, color);
+				x++;
+		}
+		y++;
+	}
 }
 
+/*
+void mandelbrot(t_lst_display **win)
+{
+	int x;
+	int y;
+	int i;
+	double x1;
+	double x2;
+	double y1;
+	double y2;
+	double c_im;
+	double c_re;
+	double old_re;
+	double old_im;
+	double new_re;
+	double new_im;
+	double zoom_x;
+	double zoom_y;
+	unsigned int color;
 
+//	printf("Debut Mandelbrot\n");
+	y = 0;
+	x1 = -2.1;
+	x2 = 1.0;
+	y1 = -1.2;
+	y2 = y1 + x2 - x1 * ((*win)->w_height / (*win)->w_width);
+	
+	zoom_x = (*win)->w_width / (x2 -x1);
+	zoom_y = (*win)->w_height /(y2 -y1);
+	while (y < (*win)->w_height)
+	{
+	//	printf("Boucle\n");
+		c_im = y / zoom_y + y1;
+		x = 0;
+		while (x < (*win)->w_width)
+		{
+		//		printf("Y: %d X: %d\n",y,x);
+				c_re = x / zoom_x+ x1;
+				
+		//		printf("C_im: %f C_re: %f\n",c_im,c_re);
+				i = 0;
+				new_im = 0;
+				old_im = 0;
+				new_re = 0;
+				old_re = 0;
+				while (i < (*win)->Max_it)
+				{
+			//		printf("Boucle it\n");
+					old_re = new_re;
+					old_im = new_im;
+					new_re = old_re * old_re - old_im * old_im + c_re;
+			//		printf("new_re %f\n",new_re);
+					new_im = 2 * old_re * old_im + c_im;
+				//	printf("new_im %f\n",new_im);
+					if ((new_re * new_re + new_im * new_im) > 4)
+						break;
+					i++;
+				}
+				color = convert_rgb(0x0, 255, 255,255 * (i < (*win)->Max_it));
+				fill_pix(win,x,y, color);
+				x++;
+		}
+		y++;
+	}
+//	printf("Fin Mandelbrot\n");
+}
+*/
+/*
 void mandelbrot(t_lst_display **win)
 {
 	int x;
@@ -60,7 +177,6 @@ void mandelbrot(t_lst_display **win)
 	double new_im;
 	unsigned int color;
 
-	printf("coucou\n");
 	y = 0;
 	
 	while (y < (*win)->w_height)
@@ -93,6 +209,7 @@ void mandelbrot(t_lst_display **win)
 						break;
 					i++;
 				}
+				//#error floating point
 				color = convert_rgb(0x0, 255 % i, i % 255,255 * (i < (*win)->Max_it));
 				fill_pix(win,x,y, color);
 				x++;
@@ -100,7 +217,7 @@ void mandelbrot(t_lst_display **win)
 		y++;
 	}
 }
-
+*/
 /*
 void mandelbrot(t_lst_display **win)
 {
